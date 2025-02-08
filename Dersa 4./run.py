89c6b634bcf5a340361af21a5e1d1369a051ac8d
Tekno Kurd <zinardevops@gmail.com>
2025-02-08 14:45:25 +0300

    ders 4

---
+++ Dersa 4./run.py
@@ -0,0 +1,47 @@
+# Liste list() []
+
+# nivîsbar her tim ji 0 ê dest bi jimartinê dikin
+# len() length dirêjî 
+"""
+a = ['sêv', 'hinar', 'gûz', True, 55]
+a[0] = 'tirî'
+a.insert(2,'pîvaz')
+print(a)
+"""
+# Dictionary dict() {}
+
+# key: value
+
+'''
+a = {
+    'username': 'teknokurd',
+    "Nav": "Zinar",
+    "Temen": 65,
+    "Ziman": [{'Kurdi':{}},"Înglîzî", 'Esperanto'],
+    'Zarok1': {
+        'Nav': "Berxwedan",
+        "Temen": 8
+    },
+    "Zarok2": {
+        "Nav": 'Kurdistan',
+        "Temen": 10
+    }}
+
+b = a
+b["Nav"] = 'Tekno Kurd'
+print(a)
+print(b)
+print(a is b)
+'''
+
+# Tuple tuple() ()
+
+'''
+a = (1,2, 'a', True)
+b = list(a)
+c = tuple(b)
+print(c)
+print(type(c))
+
+# Set set()
+'''
