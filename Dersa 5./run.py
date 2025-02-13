38c71769555c684b16095911d7c4d7abe39a937b
Tekno Kurd <zinardevops@gmail.com>
2025-02-13 14:44:08 +0300

    ders 5

---
+++ Dersa 5./run.py
@@ -0,0 +1,55 @@
+# Di pythonê de şert
+# if, elif, else
+# ku-eger, lê ku, ku nebe
+"""
+a = 5
+
+if a == 1: # False
+    print("a 1 e")
+elif a == 2: # False
+    print("a 2 ye")
+elif a == 3: # False
+    print("a 3 ye")
+elif a == 4: # False
+    print("a 4 ye")
+elif a == 5: # True
+    print("a 5 ye")
+else:
+    print("em nizanin a çi ye")
+
+
+temen = 1
+
+if temen <= 0:
+    print("tu nehatî dinê")
+elif temen < 15:
+    print("tu zarok î")
+elif temen < 30:
+    print("tu xort î")
+elif temen < 60:
+    print("tu mezin î")
+elif temen < 85:
+    print("tu pîr î")
+elif temen < 100:
+    print("tu di sekeratê de yî")
+else:
+    print("tu ejderhayî ")
+
+print("dawita kodê hat")
+
+
+
+kurd = "şêr"
+
+if (kurd == "şêr") or (kurd == "mêr"):
+    print("kurd şêr in şêr")
+
+a, b, c, = 1,2,3
+
+if (((a < b) and (b < c)) and (a < c)):
+    print("a ji b yê, b ji c yê biçûktir e")
+
+if "kurd" in "kurdistan":
+    print("kurd di nava kurdistanê de heye")
+
+"""
