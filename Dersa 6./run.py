8da6310e6d3a4b4095cc6c175612fb3d26ffde61
Tekno Kurd <zinardevops@gmail.com>
2025-02-13 15:43:15 +0300

    ders 6

---
+++ Dersa 6./run.py
@@ -0,0 +1,60 @@
+# loops - fetil, çerx, dubarekirin
+# while 
+# for
+"""
+a = 0
+while a < 100:
+    a += 1
+    if a == 55:
+        break
+    print(a)
+else:
+    print("a ji 100 î mezintir an jî wekhevê wî bû")
+
+
+
+
+
+for zinar in "kurdistan":
+    print(zinar)
+    if zinar == "s":
+        while True:
+            print(10 * zinar)
+            break
+        break
+
+
+
+listeya_me = ["kurd", "kurdî", "kurdistan"]
+
+for i in listeya_me:
+    if i == "kurdî":
+        print("bijî zimanê kurdî")
+        continue
+    print(i)
+    for x in i:
+        print(x)
+
+
+
+
+listeya_me = ["kurd", "kurdî", "kurdistan"]
+
+for i in listeya_me:
+    if i == "kurdî":
+        continue
+    print(i)
+
+
+
+for i in range(5):
+    pass
+
+if 2 < 5:
+    pass
+
+
+while True:
+    pass
+    
+ """
